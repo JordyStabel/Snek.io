ddb99b6e16dc4bd084965be88a27970b7b5a9269
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-06-04 17:01:02 +0200

    created custom error response objects & implemented more check for login/register on UserController (rest)

--- restserver/src/main/java/com/s3/SnekIO/restserver/services/UserController.java
+++ restserver/src/main/java/com/s3/SnekIO/restserver/services/UserController.java
@@ -1,12 +1,15 @@
 package com.s3.SnekIO.restserver.services;
 
-import com.mongodb.util.JSON;
+import com.s3.SnekIO.restserver.Exception.BadRequestException;
+import com.s3.SnekIO.restserver.Exception.ConflictException;
+import com.s3.SnekIO.restserver.Exception.NotFoundException;
 import com.s3.SnekIO.restserver.User;
 import com.s3.SnekIO.restserver.repositories.UserRepository;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
@@ -46,21 +49,18 @@ public class UserController {
      */
     @PostMapping(value = "/register")
     public User register(@Valid @RequestBody User user) {
-
-        // Set if email is already in use
-        if (usersRepository.findUserByEmail(user.email) != null)
+        // Check if email is already in use
+        if (usersRepository.findUserByEmail(user.email.toLowerCase()) != null)
         {
-            // TODO: Send an error message
             logger.warn("Email already in use: {}", user.email);
-            user.set_id(ObjectId.get());
-            return user;
+            throw new ConflictException("This email address is already in use.");
         }
-
-        user.set_id(ObjectId.get());
 
         // Hash the password using BCrypt
         user.setPassword(BCrypt.hashpw(user.password, BCrypt.gensalt(10)));
 
+        user.set_id(ObjectId.get());
+        user.setEmail(user.email.toLowerCase());
         usersRepository.save(user);
         logger.info("New user registered: {}", user);
         return user;
@@ -69,27 +69,28 @@ public class UserController {
     /**
      * Login a user/player
      *
-     * @param user Requires a String email, String password
+     * @param inputUser Requires a String email, String password
      * @return Newly created user with _id
      */
     @PostMapping(value = "/login")
-    public User login(@Valid @RequestBody User user) {
-        User _user = usersRepository.findUserByEmail(user.email);
+    public User login(@Valid @RequestBody User inputUser) {
+        User outputUser = usersRepository.findUserByEmail(inputUser.email.toLowerCase());
 
-        if (_user == null) {
-            logger.warn("Password incorrect");
-            return user;
+        // Check if the user exists
+        if (outputUser == null) {
+            logger.warn("User does not exist");
+            throw new NotFoundException("No user exists with this email address.");
         }
 
         // Check passwords
-        if (BCrypt.checkpw(user.password, _user.password))
+        if (BCrypt.checkpw(inputUser.password, outputUser.password))
         {
-            logger.info("User logged in: {}", _user);
-            return _user;
+            logger.info("User logged in: {}", outputUser);
+            return outputUser;
+        } else {
+            logger.warn("Wrong password");
+            throw new BadRequestException("Invalid login credentials.");
         }
-
-        logger.warn("User does not exist");
-        return user;
     }
 
     @DeleteMapping(value = "/{id}")
