546ed806d44d1e8a1d880b6c4239db1424ecbc32
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-06-04 11:40:54 +0200

    now only register unique emails, password hashing implemented, login funcion added (not final yet)

--- restserver/src/main/java/com/s3/SnekIO/restserver/RestServer.java
+++ restserver/src/main/java/com/s3/SnekIO/restserver/RestServer.java
@@ -3,12 +3,13 @@ package com.s3.SnekIO.restserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class RestServer {
 
     public static void main(String[] args) {
--- restserver/src/main/java/com/s3/SnekIO/restserver/services/UserController.java
+++ restserver/src/main/java/com/s3/SnekIO/restserver/services/UserController.java
@@ -1,11 +1,13 @@
 package com.s3.SnekIO.restserver.services;
 
+import com.mongodb.util.JSON;
 import com.s3.SnekIO.restserver.User;
 import com.s3.SnekIO.restserver.repositories.UserRepository;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -36,11 +38,57 @@ public class UserController {
         usersRepository.save(user);
     }
 
-    @PostMapping
+    /**
+     * Register a new user/player
+     *
+     * @param user Requires a String userName, String email, String password
+     * @return Newly created user with _id
+     */
+    @PostMapping(value = "/register")
     public User register(@Valid @RequestBody User user) {
+
+        // Set if email is already in use
+        if (usersRepository.findUserByEmail(user.email) != null)
+        {
+            // TODO: Send an error message
+            logger.warn("Email already in use: {}", user.email);
+            user.set_id(ObjectId.get());
+            return user;
+        }
+
         user.set_id(ObjectId.get());
+
+        // Hash the password using BCrypt
+        user.setPassword(BCrypt.hashpw(user.password, BCrypt.gensalt(10)));
+
         usersRepository.save(user);
-        logger.info("New user registered: {}", user.toString());
+        logger.info("New user registered: {}", user);
+        return user;
+    }
+
+    /**
+     * Login a user/player
+     *
+     * @param user Requires a String email, String password
+     * @return Newly created user with _id
+     */
+    @PostMapping(value = "/login")
+    public User login(@Valid @RequestBody User user) {
+        User _user = usersRepository.findUserByEmail(user.email);
+
+        if (_user == null) {
+            logger.warn("Password incorrect");
+            return user;
+        }
+
+        // Check passwords
+        if (BCrypt.checkpw(user.password, _user.password))
+        {
+            logger.info("User logged in: {}", _user);
+            return _user;
+        }
+
+        logger.warn("User does not exist");
         return user;
     }
 
@@ -53,5 +101,4 @@ public class UserController {
     public void deleteUsers() {
         usersRepository.deleteAll();
     }
-
 }
